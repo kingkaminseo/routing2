368a46703605f3dceba1f9e1894c2a0c63265d88
kingkaminseo <136690999+kingkaminseo@users.noreply.github.com>
2024-01-07 23:53:47 +0900

    routing2(세션스토리지 추가)

--- src/compontent/Contact.js
+++ src/compontent/Contact.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Contact = () => {
-    return(
-        <section className='hero'>
-            <h1>Welcome To Contact Page</h1>
-        </section>
-    )
-}
+  // 세션 스토리지에서 상태 초기화
+  const storedInputValue = sessionStorage.getItem('inputValue') || '';
+  const [inputValue, setInputValue] = useState(storedInputValue);
 
-export default Contact
+  const onChange = (e) => {
+    const value = e.target.value;
+    setInputValue(value);
+    console.log(value);
+  };
+
+  useEffect(() => {
+    // 컴포넌트가 마운트 또는 상태가 업데이트될 때 세션 스토리지에 저장
+    sessionStorage.setItem('inputValue', inputValue);
+  }, [inputValue]);
+
+  return (
+    <section className='hero'>
+      <h1>Welcome To Contact Page</h1>
+      <input type='text' value={inputValue} onChange={onChange} />
+    </section>
+  );
+};
+
+export default Contact;
